93c1477eda8547178ffe082c3125e436daea28d3
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-09-15 16:18:18 -0500

    Initial commit

---
+++ test/test_average_scores.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest import mock
+
+from format_output import average_scores
+
+class MyTestCase(unittest.TestCase):
+    def test_average(self):
+        with mock.patch(90, side_effects=[85, 90, 95]):
+            assert average_scores.average() == 90
+
+
+if __name__ == '__main__':
+    unittest.main()
