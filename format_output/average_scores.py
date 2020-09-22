003facd4adf5f28163b0ac1a84865d8a8ea30f19
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-09-22 13:09:16 -0500

    Input validation test passes

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -7,17 +7,24 @@ Module 3 - average scores
 """
 
 
-def average() :
-    test1 = input("Enter test 1 score: ")
-    test2 = input("Enter test 2 score: ")
-    test3 = input("Enter test 3 score: ")
-    average_score = (int(test1) + int(test2) + int(test3))/3
-    return average_score
+def average(test1, test2, test3):
+    if (test1 > 0 and test2 > 0 and test3 > 0):
+        average_score = (int(test1) + int(test2) + int(test3)) / 3
+    else:
+        raise ValueError
+    return round(average_score, 2)
 
 
 if __name__ == '__main__':
     lastName = input("Enter your last name: ")
     firstName = input("Enter your first name: ")
     age = input("Enter your age: ")
-    average_scores = average()
+    test1 = input("Enter test 1 score: ")
+    test2 = input("Enter test 2 score: ")
+    test3 = input("Enter test 3 score: ")
+    try:
+        average_scores = average(int(test1), int(test2), int(test3))
+    except Exception:
+        raise(ValueError)
+        print("There was a problem finding the average score")
     print("{}, {} age: {} Average grade: {}".format(lastName, firstName, age, average_scores))
